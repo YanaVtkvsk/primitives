03fdee635f0345acadcd9476ff93bdb79aae818a
Yana Vitkovskay <vitkovskaya.yana@gmail.com>
2022-09-09 15:37:10 +0300

    Primitives

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 100; // цена за билет
+        int rub = 20; // потраченные рубли, за которые начисляют 1 милю
+        int mil = price / rub;
+
+        System.out.println((mil) + " Миль за 1 купленный билет " );
+
+    }
+}
